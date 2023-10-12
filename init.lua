8c414c2f0230a2d177ac740e87e5ce04c0c09055
gabe <gabeklav@fastmail.com>
2023-10-12 01:13:48 -0400

    add some default opts rec'd in course

--- init.lua
+++ init.lua
@@ -213,10 +213,14 @@ require('lazy').setup(
 -- NOTE: You can change these options as you wish!
 
 -- Set highlight on search
-vim.o.hlsearch = false
+vim.o.hlsearch = true
 
--- Make line numbers default
+-- Keep the cursor from reaching the edge
+vim.o.scrolloff = 8
+
+-- Make relative line numbers default
 vim.wo.number = true
+vim.wo.relativenumber = true
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
