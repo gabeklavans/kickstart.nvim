88bc29e1690133ea9584cf2ba3ebe11014423bb1
gabe <gabeklav@fastmail.com>
2023-10-18 19:28:09 -0400

    Switch to ayu_dark theme

--- init.lua
+++ init.lua
@@ -128,11 +128,10 @@ require('lazy').setup({
   },
 
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'Shatur/neovim-ayu',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'ayu'
     end,
   },
 
@@ -143,7 +142,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'ayu_dark',
         component_separators = '|',
         section_separators = '',
       },
