ae94ee78dae4e39d71521eaad3714e8afd238b80
gabe <gabeklav@fastmail.com>
2023-10-24 00:39:07 -0400

    add formatter plugin (with my own remap!!)

--- lua/custom/plugins/format.lua
+++ lua/custom/plugins/format.lua
@@ -1,3 +1,8 @@
 return {
   'sbdchd/neoformat',
+  config = function ()
+    vim.g.neoformat_try_node_exe = 1
+
+    vim.keymap.set('n', '<leader>f', ":Neoformat<CR>", { desc = '[F]ormat Buffer' })
+  end
 }
