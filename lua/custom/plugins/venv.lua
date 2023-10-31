b053dd387986b0eab0d433b5a0133b3ab867b445
Gabe <gabriel.k@yobeinc.com>
2023-10-31 11:15:08 -0400

    add python venv selector

---
+++ lua/custom/plugins/venv.lua
@@ -0,0 +1,14 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+  opts = {
+    -- Your options go here
+    -- name = "venv",
+    -- auto_refresh = false
+  },
+  event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  keys = {
+    { "<leader>vs", "<cmd>:VenvSelect<cr>", desc = "[V]env [S]elect"},
+    { "<leader>vc", "<cmd>:VenvSelectCached<cr>", desc = "[V]env [C]ached" }
+  }
+}
