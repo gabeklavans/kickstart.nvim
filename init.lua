90c0e3e654800be54e1064bd834f88c4547b4787
gabe <gabriel.k@yobeinc.com>
2023-12-20 11:39:57 -0500

    don't ignore .gitignored files

--- init.lua
+++ init.lua
@@ -332,7 +332,7 @@ require('telescope').setup {
   pickers = {
     live_grep = {
       additional_args = function (opts)
-        return {"--hidden"}
+        return {"--hidden", "--no-ignore-vcs"}
       end
     }
   }
