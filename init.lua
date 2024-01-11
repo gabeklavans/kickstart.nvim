40a8f8e754b7e723fb8bace5e29d21f6cd060745
gabe <gabriel.k@yobeinc.com>
2024-01-11 12:28:23 -0500

    disable auto header imports for clangd

--- init.lua
+++ init.lua
@@ -563,7 +563,9 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  clangd = {},
+  clangd = {
+    cmd = { "clangd --header-insertion=never" }
+  },
   gopls = {},
   pyright = {},
   rust_analyzer = {},
